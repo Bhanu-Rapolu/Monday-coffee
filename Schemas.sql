d6fe5fa6294fbe92ff1fa9d4986866c68eed7f20
Bhanu-Rapolu <rapolubhanu1@gmail.com>
2025-05-11 21:12:50 +0530

    Update and rename Schemas[1].sql to Schemas.sql

--- Schemas.sql
+++ Schemas.sql
@@ -1,11 +1,5 @@
 -- Monday Coffee SCHEMAS
 
-DROP TABLE IF EXISTS sales;
-DROP TABLE IF EXISTS customers;
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS city;
-
--- Import Rules
 -- 1st import to city
 -- 2nd import to products
 -- 3rd import to customers
